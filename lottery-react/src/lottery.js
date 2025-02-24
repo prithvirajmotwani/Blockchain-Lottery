f6dae68b9ee5c21c146c106d0b40c3bdc0f4c41c
prithvirajmotwani <prithviraj182002@gmail.com>
2025-02-24 19:32:49 +0500

    Complete Project

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,10 @@
+import web3 from './web3';
+
+const address = '0xC0354dAe3DD90830a4Be7BebCCA41f75aea1b5B2';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+
+export default new web3.eth.Contract(abi, address); //exports completer copy of our contract that we can work with our react app
+
+
